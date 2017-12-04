c9708bf46798b3b5069069bd9c9551cd63bbde0c
Unknown <supervideoman@ya.ru>
2017-12-04 14:29:04 +0300

    Auto stash before merge of "master" and "origin/master"

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+	// write your code here
+        new Five().run();
         new One();
         new BestSegaMegaDriveClass(1 + 2);
     }
