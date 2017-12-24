9a4105234119014cb521d8f978e2bd3b2bbac8b7
Aleksandr <supervideoman@ya.ru>
2017-12-24 20:28:21 +0300

    add method digestToChar, as xls

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,16 +1,14 @@
 package com.company;
+import com.utils.*;
 
-import digist.*;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        
-        new Two().run();
-        new One();
-        new BestSegaMegaDriveClass(1 + 2);
-        MySecondClass my = new MySecondClass();
-        System.out.println("End!");
+        for(int i = 0; i<800; i++)
+        {
+            System.out.println(i+" = "+DigestToChar.make(i));
+        }
+
     }
 }
---
+++ src/com/utils/DigestToChar.java
@@ -0,0 +1,21 @@
+package com.utils;
+
+public class DigestToChar {
+    public static String make(int digest){
+        String result = "";
+        StringBuilder str = new StringBuilder("");
+
+        int systemDigets = 26;
+        while(digest !=0 || digest>systemDigets) {
+            if(digest% systemDigets == 0) {
+                str.append((char) (systemDigets + 64));
+                digest--;
+            }
+            else
+            str.append((char) (digest % systemDigets + 64));
+            digest = digest / systemDigets;
+        }
+        result = str.reverse().toString();
+        return result;
+    }
+}
