db8e3ad769163e1253868bb9b61ae7d5e0e7be5f
luser <nomail@mail.com>
2017-12-25 09:46:06 +0300

    add makeFrom0

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,6 @@ import com.utils.*;
 public class Main {
 
     public static void main(String[] args) {
-
         for(int i = 0; i<800; i++)
         {
             System.out.println(i + " = " + DigestToChar.makeFrom0(i));
