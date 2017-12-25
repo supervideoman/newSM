d402b44f815ecd2ac49efd5d9201269d84d2bebe
Alexandr <supervideoman@ya.ru>
2017-12-25 17:38:25 +0300

    Comments is a useful and needed thing

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,13 @@
 package com.company;
 import com.utils.*;
 
+
 public class Main {
     public static void main(String[] args) {
         for(int i = 0; i<800; i++) {
             System.out.println(i + " = " + DigestToChar.makeFrom0(i));
         }
+        //FilenameUtils.getExtension("/path/to/file/foo.txt"); /
+
     }
 }
---
+++ src/com/utils/FilesNameTools.java
@@ -0,0 +1,10 @@
+package com.utils;
+
+public class FilesNameTools {
+    public static String getLastName(String path){
+        String result = "";
+
+        return result;
+
+    }
+}
