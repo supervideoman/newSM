2a466a09f07e2fb94c7e987cdf56ee174c5dbc41
Alexandr <supervideoman@ya.ru>
2017-12-25 16:32:02 +0300

    Comments is a useful and needed thing

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,7 @@ import com.utils.*;
 public class Main {
     public static void main(String[] args) {
         for(int i = 0; i<800; i++) {
+
             System.out.println(i + " = " + DigestToChar.makeFrom0(i));
         }
     }
