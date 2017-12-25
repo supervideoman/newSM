6d14b6bfca72cd7919444fec385ffb3c87d68aa9
luser <nomail@mail.com>
2017-12-25 09:43:33 +0300

    add makeFrom0

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,9 +5,10 @@ import com.utils.*;
 public class Main {
 
     public static void main(String[] args) {
+
         for(int i = 0; i<800; i++)
         {
-            System.out.println(i + " = " + DigestToChar.make(i));
+            System.out.println(i + " = " + DigestToChar.makeFrom0(i));
         }
     }
 }
--- src/com/utils/DigestToChar.java
+++ src/com/utils/DigestToChar.java
@@ -17,4 +17,21 @@ public class DigestToChar {
         result = str.reverse().toString();
         return result;
     }
+    public static String makeFrom0(int digest) {
+        digest++;
+        String result;
+        StringBuilder str = new StringBuilder("");
+        int systemDigets = 26;
+        while((digest != 0) || (digest > systemDigets)) {
+            if(digest% systemDigets == 0) {
+                str.append((char) (systemDigets + 64));
+                digest--;
+            }
+            else
+                str.append((char) (digest % systemDigets + 64));
+            digest = digest / systemDigets;
+        }
+        result = str.reverse().toString();
+        return result;
+    }
 }
