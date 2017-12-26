2467c9be5e83062cb4577ddfd59bddd5c9f42524
Alexandr <supervideoman@ya.ru>
2017-12-26 11:22:42 +0300

    add method addNameFile

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,13 +1,24 @@
 package com.company;
 import com.utils.*;
 
+import java.util.Date;
+
 
 public class Main {
     public static void main(String[] args) {
         for(int i = 0; i<800; i++) {
             System.out.println(i + " = " + DigestToChar.makeFrom0(i));
         }
+
+
         //FilenameUtils.getExtension("/path/to/file/foo.txt"); /
-        System.out.println(FilesNameTools.getFileExtension("\\fs\\123.4.5r.fi\\fsfs.txt"));
+        long  start = new Date().getTime();
+        for(int i=0; i<1_000_000; i++) {
+            //FilesNameTools.getFileExtension("\\fs\\123.4.5r.fi\\fsfs.txt");
+            FilesNameTools.addNameFile("\\fs\\123.4.5r.fi\\fsfs.txt", "LALA_"+i);
+            FilesNameTools.addNameFile("\\fs\\12345rfi\\fsfstxt", "LALA2_"+i);
+            FilesNameTools.addNameFile("", "LALA3_"+i);
+        }
+        System.out.println("Первый алгоритм отработал за " + (new Date().getTime()- start)+"мс");
     }
 }
--- src/com/utils/FilesNameTools.java
+++ src/com/utils/FilesNameTools.java
@@ -8,4 +8,14 @@ public class FilesNameTools {
         //System.out.println(preResult.substring(0,preResult.lastIndexOf(".") + 1));
         return preResult.substring(preResult.lastIndexOf(".") + 1);
     }
+    public static String addNameFile(String path, String addedName){
+        StringBuilder preResult = new StringBuilder();
+        preResult.append(path);
+
+        int tmp = (preResult.lastIndexOf(".")==-1)?preResult.length():preResult.lastIndexOf(".");
+        //System.out.println(tmp);
+        preResult.insert(tmp, addedName);
+        return preResult.toString();
+    }
+
 }
