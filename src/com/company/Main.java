04007da90a0eec07f41107056541fb719c641b42
Mikhail Ruchenkin <ruchenkin@vistgroup.ru>
2017-12-26 11:33:31 +0300

    1. Добавлен метод работы со строкой основанный на split 2. Добавлен тест на сравнение работы двух методов

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,11 +3,38 @@ import com.utils.*;
 
 
 public class Main {
-    public static void main(String[] args) {
-        for(int i = 0; i<800; i++) {
-            System.out.println(i + " = " + DigestToChar.makeFrom0(i));
+    public static void main(String[] args)
+    {
+        StringBuilder[] testString = new StringBuilder[100000];
+        for(int i = 0; i < testString.length; i++)
+        {
+            testString[i] = new StringBuilder("");
+            int l = Main.getRandom(4, 11);
+            for(int j = 0; j < l; j++)
+            {
+                StringBuilder str = new StringBuilder("");
+                int l2 = Main.getRandom(3, 10);
+                for(int k = 0; k < l2; k++)
+                {
+                    int r = Main.getRandom(0, 100);
+                    if(r > 70) str.append((char)Main.getRandom(65, 91));
+                    else if(r > 5) str.append((char)Main.getRandom(97, 121));
+                    else str.append((char)46);
+                }
+                if(j == l - 1) testString[i].append("name_file." + str);
+                else testString[i].append(str + "/");
+            }
+            //System.out.println(testString[i]);
         }
-        //FilenameUtils.getExtension("/path/to/file/foo.txt"); /
-        System.out.println(FilesNameTools.getFileExtension("\\fs\\123.4.5r.fi\\fsfs.txt"));
+        long start = System.currentTimeMillis();
+        for(int i = 0; i < testString.length; i++) FilesNameTools.getFileNameAndExtension(testString[i].toString(), "_fuck");
+        System.out.println(System.currentTimeMillis() - start);
+        start = System.currentTimeMillis();
+        for(int i = 0; i < testString.length; i++) FilesNameTools.getFileExtension(testString[i].toString(), "_fuck");
+        System.out.println(System.currentTimeMillis() - start);
+    }
+    private static int getRandom(int a, int b)
+    {
+        return (int) (Math.random() * (b - a) + a);
     }
 }
--- src/com/utils/FilesNameTools.java
+++ src/com/utils/FilesNameTools.java
@@ -1,11 +1,23 @@
 package com.utils;
 
 public class FilesNameTools {
-    public static String getFileExtension(String path) {
+    public static String getFileExtension(String path, String add) {
         StringBuilder preResult = new StringBuilder();
         preResult.append(path);
         // Имя полное(путь) без расширения
         //System.out.println(preResult.substring(0,preResult.lastIndexOf(".") + 1));
-        return preResult.substring(preResult.lastIndexOf(".") + 1);
+        int end = preResult.lastIndexOf(".");
+        return preResult.substring(0, end) + add + preResult.substring(end);
+    }
+    public static String getFileNameAndExtension(String path, String add)
+    {
+        StringBuilder preResult = new StringBuilder();
+        String[] timeStr = path.split("\\.");
+        for (int i = 0; i < timeStr.length - 1; i++)
+            if(i == timeStr.length - 2)
+                preResult.append(timeStr[i] + add + "." + timeStr[i + 1]);
+            else preResult.append(timeStr[i] + ".");
+        if(timeStr.length == 1) preResult.append(timeStr[0] + add);
+        return preResult.toString();
     }
 }
